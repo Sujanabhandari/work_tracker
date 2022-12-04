be1e5c0d091d96ee198d19cc459b1779a6e5095d
Sujana Bhandari <sujana6589@gmail.com>
2022-12-04 11:37:57 +0100

    Basic css styling and add InputField

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import InputField from './components/InputField';
+import { Todo } from './model';
 
-function App() {
+const App: React.FC = () => {
+
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<(Todo[])>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo){
+      setTodos([...todos, {id:Date.now(), todo:todo, isDone:false}])
+      setTodo("");
+    }
+  };
+
   return (
-    <div className="App">
-      I will help you to track your work.
+    <div className='App'>
+      <span className='heading'>Task Tracker</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
     </div>
   );
 }
